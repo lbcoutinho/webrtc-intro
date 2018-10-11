ef626f718e1bf1ed1bc8b35650bbeafd8a610d63
Leandro Coutinho <lecouti@motorola.com>
2018-10-11 15:45:43 -0300

    Socket.io setup and simple chat

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const path = require('path');
 
 // Public folder and HTML render engine setup
 app.use(express.static(path.join(__dirname, 'public')));
@@ -8,6 +10,7 @@ app.set('views', path.join(__dirname, 'public'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
+// Express routes
 app.get('/intro', (req, res) => {
   res.render('intro/intro.html');
 });
@@ -16,5 +19,23 @@ app.get('/', (req, res) => {
   res.render('index.html');
 });
 
+// Socket.io setup
+io.on('connection', client => {
+  console.log(`Client connected: ${client.id}`);
+
+  client.on('join-room', room => {
+    console.log(`join-room: ${room}`);
+
+    client.join(room, () => {
+      console.log('client rooms = ', client.rooms);
+      io.to(room).emit('new-client', `New client in the room ${room}.`);
+    });
+  });
+
+  client.on('send', data => {
+    io.to(data.room).emit('message', data);
+  });
+});
+
 const port = 3000;
-app.listen(port, () => console.log(`Server started on port ${port}`));
+server.listen(port, console.log(`Server started on port ${port}`));
--- public/index.js
+++ public/index.js
@@ -1,92 +1,69 @@
 // HTML elements
-const video = document.querySelector('video');
-const videoSelector = document.querySelector('#videoSelector');
-const audioSelector = document.querySelector('#audioSelector');
+const myVideo = document.querySelector('#myVideo');
+const peerVideo = document.querySelector('#peerVideo');
+
+// Chat HTML elements
+const name = document.querySelector('#name');
+const message = document.querySelector('#message');
+const sendBtn = document.querySelector('#send');
+const chat = document.querySelector('#chat');
+const ROOM = '#1';
 
 // Steam initialization
 function startStream() {
   console.log('startStream');
 
-  const audioSource = audioSelector.value || audioSelector.options[0].value;
-  const videoSource = videoSelector.value || videoSelector.options[0].value;
-
   // Other constraints option: https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
   const constraints = {
-    audio: false,
-    video: false 
+    audio: true,
+    video: { width: 240, height: 240 }
   };
 
   navigator.mediaDevices
     .getUserMedia(constraints)
     .then(stream => {
       console.log('startStream#then: ', stream);
-      video.src = window.URL.createObjectURL(stream);
-      video.load();
-      video.play();
+      myVideo.src = window.URL.createObjectURL(stream);
+      myVideo.load();
+      myVideo.play();
     })
     .catch(err => {
       console.log('Error on mediaDevices.getUserMedia: ', err);
-      // User reject share request
-      // MediaStreamError { name: "SecurityError", message: "The operation is insecure.", constraint: "", stack: "" }
-
-      // Stream already in user in another browser
-      // MediaStreamError { name: "SourceUnavailableError", message: "Failed to allocate videosource", constraint: "", stack: "" }
-
-      // Wrong definition of constraints
-      // MediaStreamError { name: "OverconstrainedError", message: "Constraints could be not satisfied.", constraint: "deviceId", stack: "" }
     });
 }
 
-// Setup available devices (cameras and microphones) in selectors
-function setDevices() {
-  console.log('setDevices');
-  if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
-    console.log('enumerateDevices() not supported.');
-    document.querySelector('#audioDevices').style.visibility = 'hidden';
-    document.querySelector('#videoDevices').style.visibility = 'hidden';
-    return;
-  }
-
-  videoSelector.onchange = startStream;
-  audioSelector.onchange = startStream;
-
-  navigator.mediaDevices
-    .enumerateDevices()
-    .then(devices => {
-      console.log('setDevices#then: ', devices);
-      devices.forEach(({ deviceId, kind, label }) => {
-        const opt = document.createElement('option');
-        opt.value = deviceId;
-        opt.text = label || `${kind} - ${deviceId}`;
-
-        // Add only known device kinds and  unique values
-        if (
-          kind == 'videoinput' &&
-          videoSelector.innerHTML.indexOf(`value="${deviceId}"`) == -1
-        ) {
-          videoSelector.appendChild(opt);
-        } else if (
-          kind == 'audioinput' &&
-          audioSelector.innerHTML.indexOf(`value="${deviceId}"`) == -1
-        ) {
-          audioSelector.appendChild(opt);
-        } else if (kind != 'videoinput' && kind != 'audioinput') {
-          console.log(
-            `Device type not recognized, ignoring device: kind: ${kind}, id: ${deviceId}`
-          );
-        }
+function setupChat(socket) {
+  sendBtn.addEventListener(
+    'click',
+    e => {
+      socket.emit('send', {
+        name: name.value,
+        message: message.value,
+        room: ROOM
       });
+      e.preventDefault();
+    },
+    false
+  );
+}
 
-      // Start stream only after devices are retrieved and set
-      startStream();
-    })
-    .catch(err => {
-      console.log('mediaDevices.enumerateDevices: ', err);
-    });
+function displayMessage(msg) {
+  chat.innerHTML += msg + '<br>';
 }
 
 // Invoke functions
 window.onload = function() {
   console.log('--- Start');
-  setDevices();
+  const socket = io('http://localhost:3000');
+
+  startStream();
+  setupChat(socket);
+
+  socket.emit('join-room', ROOM);
+  socket.on('new-client', msg => {
+    displayMessage(msg);
+  });
+  socket.on('message', data => {
+    displayMessage(`${data.name}: ${data.message}`);
+  });
 };
