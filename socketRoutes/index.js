1b4fcaeca4c0ec28bb582550f9f4b365b264779f
Leandro Coutinho <lecouti@motorola.com>
2018-10-16 13:39:26 -0300

    Signaling server refactored

--- socketRoutes/index.js
+++ socketRoutes/index.js
@@ -1,6 +1,5 @@
 module.exports = (io, socket) => {
   socket.on('join-room', room => {
-
     socket.join(room, () => {
       io.to(room).emit('new-client', `New client in the room ${room}.`);
     });
@@ -10,24 +9,16 @@ module.exports = (io, socket) => {
     io.to(data.room).emit('message', data);
   });
 
-  socket.on('start-signaling', data => {
-    // Use socket.to so all clients in the room receive the message except the sender
-    socket.to(data.room).emit('start-signaling-received', data);
-  });
-
   socket.on('new-ice-candidate', data => {
+    // Use socket.to so all clients in the room receive the message except the sender
     socket.to(data.room).emit('ice-candidate-received', data);
   });
 
-  socket.on('sdp-offer', data => {
-    socket.to(data.room).emit('sdp-offer-received', data);
+  socket.on('send-sdp', data => {
+    socket.to(data.room).emit('sdp-received', data);
   });
-
-  // socket.on('signal', data => {
-  //   socket.to(data.room).emit('signaling-message', data);
-  // });
 
   socket.on('log', data => {
     io.to(data.room).emit('log', data);
-  })
-}
+  });
+};
