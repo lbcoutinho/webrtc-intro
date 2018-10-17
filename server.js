8e0e304640cfcfb5314099f0089f51d0c7f35a8f
lbcoutinho <lbcoutinho.blm@gmail.com>
2018-10-17 12:39:28 -0300

    Fix server port for Heroku

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ io.on('connection', client => {
   require('./socketRoutes')(io, client);
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 server.listen(port, console.log(`Server started on port ${port}`));
